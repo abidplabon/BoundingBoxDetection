6e10c19e1b87a2c9403db8785a5d94d4741da32a
abidahammed <54079464+abidplabon@users.noreply.github.com>
2022-01-28 01:22:20 +0600

    Data update on google sheet

--- app.py
+++ app.py
@@ -4,8 +4,26 @@ from uuid import uuid4
 from flask import Flask, render_template, request,send_from_directory
 ##import tensorflow as tf
 global graph,model
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 ##graph = tf.get_default_graph()
 ##import detection
+
+# define the scope
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+# add credentials to the account
+creds = ServiceAccountCredentials.from_json_keyfile_name('helloworld.json', scope)
+
+# authorize the clientsheet 
+client = gspread.authorize(creds)
+
+
+# get the instance of the Spreadsheet
+sheet = client.open('PneumoniaDataCollection')
+# get the first sheet of the Spreadsheet
+sheet_instance = sheet.get_worksheet(0)
 
 
 result = 0
@@ -41,6 +59,8 @@ def upload():
     print(imageid)
     clinicname=request.form.get("clinicname")
     print(clinicname)
+    
+    
     # Image folder 
     target = os.path.join(APP_ROOT, 'images/')
     # target = os.path.join(APP_ROOT, 'static/')
@@ -59,6 +79,17 @@ def upload():
         
         upload.save(destination)
     
+    elements=[file_name,paitentname,paitentage,gender,imageid,clinicname]
+    data=pd.DataFrame.from_dict(elements)
+    # get all the records of the data
+    #records_data = sheet_instance.get_all_records()
+    # convert the json to dataframe
+    #records_df = pd.DataFrame.from_dict(records_data)
+
+    # view the top records
+    #recordhead=records_df.head()
+
+    sheet_instance.insert_cols(data.values.tolist())
     ##result = ml.detect(destination)
     
 
